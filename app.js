0769c15553344d65222f52b91a594a43504a8e3f
John Atkins <johnatkins1999@yahoo.com>
2017-07-19 11:09:58 -1000

    refactor

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('Body-Parser');
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
 const app = express();
 const words = { buzzwords: [] };
-let userScore = 0;
+const players = { 'score': 0 };
 
-app.use(express.static('express-todo-api'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/buzzwords', getWords);
+app.post('/buzzword', postWord);
+app.put('/buzzword', putWord);
+app.delete('/buzzword', deleteBuzzword);
+app.post('/reset', resetBuzzwords);
+app.listen(9000);
 
-app.get('/buzzwords', (req, res, next) => {
-  res.json(words);
-  next();
-});
-
-app.post('/buzzword', urlencodedParser, (req, res, next) => {
+function postWord(req, res) {
   if (!req.body) return res.sendStatus(400);
   let buzzword = req.body.buzzword;
   let points = Number(req.body.points);
@@ -25,60 +25,41 @@ app.post('/buzzword', urlencodedParser, (req, res, next) => {
   let buzzObj = words.buzzwords;
   buzzObj.push({ buzzword, points, heard });
   res.send({ success: true });
-  next();
-});
-
-app.put('/buzzword', urlencodedParser, (req, res, next) => {
-  if (!req.body) return res.sendStatus(400);
-  let score = updateBuzzword(req.body.buzzword);
-  if (score !== false){
-    res.send( { 'success': true, 'newScore': score } );
-  } else {
-    res.sendStatus(400);
-  }
-  next();
-});
-
-app.delete('/buzzword', urlencodedParser, (req, res, next) => {
-  if (!req.body) return res.sendStatus(400);
-  let del = deleteBuzzword(req.body.buzzword);
-  if (del !== false){
-    res.send( { 'success': true } );
-  } else {
-    res.sendStatus(400);
-  }
-  next();
-});
-
-app.post('/reset', (req, res, next) => {
-  words.buzzwords = [];
-  res.send({ reset: true });
-  next();
-});
-
-
-app.listen(9000);
-
-function updateBuzzword( bw ) {
-  let wordArr = words.buzzwords;
-  for (var i = 0; i < wordArr.length; i++) {
-    if (wordArr[i].buzzword === bw){
-      wordArr[i].heard = true;
-      userScore += wordArr[i].points;
-      return wordArr[i].points;
-    }
-  }
-  return false;
 }
 
-function deleteBuzzword( bw ) {
+function putWord(req, res) {
+  if (!req.body || !req.body.buzzword || !req.body.heard) return res.sendStatus(400);
+
   let wordArr = words.buzzwords;
   for (var i = 0; i < wordArr.length; i++) {
-    console.log(wordArr[i].buzzword);
-    if (wordArr[i].buzzword === bw){
-      wordArr.splice(wordArr.indexOf(wordArr[i]));
-      return true;
+    if (wordArr[i].buzzword === req.body.buzzword){
+      wordArr[i].heard = true;
+      players.score += wordArr[i].points;
+      res.send({ 'success': true, 'newScore': players.score });
+      return;
     }
   }
-  return false;
+  res.sendStatus(404);
+}
+
+function getWords(req, res) {
+  res.json(words);
+}
+
+function deleteBuzzword(req, res) {
+  if (!req.body) return res.sendStatus(400);
+  let wordArr = words.buzzwords;
+  for (var i = 0; i < wordArr.length; i++) {
+    if (wordArr[i].buzzword === req.body.buzzword){
+      wordArr.splice(wordArr.indexOf(wordArr[i]), 1);
+      res.send( { 'success': true } );
+      return;
+    }
+  }
+  res.sendStatus(404);
+}
+
+function resetBuzzwords(req, res) {
+  words.buzzwords = [];
+  res.send({ reset: true });
 }
